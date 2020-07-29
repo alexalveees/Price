6f736b1f9ee4ded589c4cc43cf45d23f981d8a84
Alex Alves <alexalvesfd@hotmail.com>
2020-07-28 22:16:24 -0300

    trocando porta para ser via variavel de ambiente e corringido URI do js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 console.log('javascript no frontend')
 
-fetch('http://localhost:3000/cotacoes?ativo=aapl').then((response) => { 
+fetch('/cotacoes?ativo=aapl').then((response) => { 
     response.json().then((data) => {
         console.log(data);           
     })
